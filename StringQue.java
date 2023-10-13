90ef2b8eaa97bf704ea36bfd20dadeb930a794da
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-10-13 11:44:00 +0530

    Add files via upload

---
+++ StringQue.java
@@ -0,0 +1,58 @@
+/*
+Write a Program to take a user-defined input in which 
+1.All Upper Case letters should be shifted left by 2 characters
+2.All Lower Case letters should be shifted left by 3 characters
+ */
+package com.string;
+import java.util.Scanner;
+public class StringQue 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String:");
+		String input=sc.nextLine();
+		
+		StringBuilder output=new StringBuilder();
+		
+		for(char character : input.toCharArray())
+		{
+			// if character is Uppercase then character is decremented by 2
+			if(Character.isUpperCase(character))
+			{
+				if (character == 'A') 
+				{
+                    output.append('Y');
+                } 
+				else 
+				{
+                    output.append((char) (character - 2));
+                }
+			}
+			// if character is Lowercase then character is decremented by 2
+			else if(Character.isLowerCase(character))
+			{
+				if (character == 'a') 
+				{
+                    output.append('Z');
+                } 
+				else if (character == 'b') 
+				{
+					output.append('Y');
+                } 
+				else 
+				{
+					output.append((char) (character - 3));
+                }
+			}
+			else
+			{
+				output.append(character);
+			}
+			
+		}
+		System.out.println("Output: "+output.toString());
+	}
+}
